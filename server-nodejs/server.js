e2c88f193bbad8903d3e5e33bd3fe9fc9e227ce5
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-12-21 16:40:43 +0100

    server nodejs connection to mysql

---
+++ server-nodejs/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'zakaria',
+    password: 'root1234',
+    database: 'DepenseTelecom',
+    insecureAuth: true
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('MySQL connection error:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+app.post('/performQuery', (req, res) => {
+    const sql = req.body.sql;
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('MySQL query error:', err.stack);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

58cfe76d6eb226d309f9ce2355ac1811cc9e6980
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-12-26 23:30:08 +0100

    fix overflow and add logout

--- app/src/main/java/com/gi3/mesdepensestelecom/MainActivity.java
+++ app/src/main/java/com/gi3/mesdepensestelecom/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         if (navigationView != null) {
             mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_settings, R.id.nav_login, R.id.nav_register)
+                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_settings, R.id.nav_login, R.id.nav_register, R.id.nav_logout)
                     .setOpenableLayout(binding.drawerLayout)
                     .build();
             NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = binding.appBarMain.contentMain.bottomNavView;
         if (bottomNavigationView != null) {
             mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_login, R.id.nav_register)
+                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_login, R.id.nav_register, R.id.nav_logout)
                     .build();
             NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
             NavigationUI.setupWithNavController(bottomNavigationView, navController);
@@ -92,10 +92,32 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         boolean result = super.onCreateOptionsMenu(menu);
         NavigationView navView = findViewById(R.id.nav_view);
-        if (navView == null && mAppBarConfiguration != null && !mAppBarConfiguration.getTopLevelDestinations().contains(R.id.nav_login) && !mAppBarConfiguration.getTopLevelDestinations().contains(R.id.nav_register)) {
-            getMenuInflater().inflate(R.menu.overflow, menu);
+
+        if (navView == null && mAppBarConfiguration != null) {
+            // Check the destination ID inside the destination changed listener
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+            boolean isLoginOrRegisterDestination = isLoginOrRegisterDestination(navController.getCurrentDestination().getId());
+
+            // Check if the current destination is login or register
+            if (!isLoginOrRegisterDestination) {
+                getMenuInflater().inflate(R.menu.overflow, menu);
+            }
+        } else if (navView != null && mAppBarConfiguration != null) {
+            // Check the destination ID inside the destination changed listener
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+            boolean isLoginOrRegisterDestination = isLoginOrRegisterDestination(navController.getCurrentDestination().getId());
+
+            // Check if the current destination is login or register
+            if (!isLoginOrRegisterDestination) {
+                getMenuInflater().inflate(R.menu.overflow, menu);
+            }
         }
         return result;
+    }
+
+    // Helper method to check if the current destination is login or register
+    private boolean isLoginOrRegisterDestination(int destinationId) {
+        return destinationId == R.id.nav_login || destinationId == R.id.nav_register;
     }
 
     // Handle item selection from the overflow menu
@@ -104,6 +126,10 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.nav_settings) {
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
             navController.navigate(R.id.nav_settings);
+        }
+        if (item.getItemId() == R.id.nav_logout) {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_login);
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/gi3/mesdepensestelecom/ui/logout/LogoutFragment.java
@@ -0,0 +1,49 @@
+package com.gi3.mesdepensestelecom.ui.logout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.gi3.mesdepensestelecom.R;
+import com.gi3.mesdepensestelecom.databinding.FragmentLogoutBinding;
+
+public class LogoutFragment extends Fragment {
+
+    private FragmentLogoutBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LogoutViewModel logoutViewModel =
+                new ViewModelProvider(this).get(LogoutViewModel.class);
+
+        binding = FragmentLogoutBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textLogout;
+        logoutViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final Button logoutButton = binding.logoutButton;
+        logoutButton.setOnClickListener(view -> {
+            // Perform logout actions here, such as clearing user sessions or preferences
+
+            // Navigate back to the login screen
+            Navigation.findNavController(view).navigate(R.id.nav_login);
+        });
+        
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

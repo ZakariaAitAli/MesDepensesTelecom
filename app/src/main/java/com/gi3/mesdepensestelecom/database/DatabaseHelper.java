1badc71a7ee7ad025a64b65ad309ca52da5e7b1c
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2024-01-02 18:44:01 +0100

    Implement RepositoryUser for login/register

--- app/src/main/java/com/gi3/mesdepensestelecom/database/DatabaseHelper.java
+++ app/src/main/java/com/gi3/mesdepensestelecom/database/DatabaseHelper.java
@@ -6,49 +6,52 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+import com.gi3.mesdepensestelecom.Models.User;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String databaseName = "Depense.db";
 
-    public DatabaseHelper(@Nullable Context context) {
+    public DatabaseHelper(Context context) {
         super(context, databaseName, null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase MyDatabase) {
-        MyDatabase.execSQL("create Table users(id INTEGER primary key, username TEXT, password TEXT)");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create Table users(id INTEGER primary key, username TEXT, password TEXT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
-        MyDB.execSQL("drop Table if exists users");
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop Table if exists users");
+        onCreate(db);
     }
 
-    public Boolean insertData(String username, String password) {
-        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+    public long insertUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("username", username);
-        contentValues.put("password", password);
-        long result = MyDatabase.insert("users", null, contentValues);
-        return result != -1;
+        contentValues.put("username", user.username);
+        contentValues.put("password", user.password);
+        long result = db.insert("users", null, contentValues);
+        db.close();
+        return result;
     }
 
-    public Boolean checkUsername(String username) {
-        SQLiteDatabase myDatabase = this.getWritableDatabase();
-        Cursor cursor = myDatabase.rawQuery("SELECT * FROM users WHERE username = ?", new String[]{username});
+    public boolean checkUsername(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE username = ?", new String[]{username});
         boolean usernameExists = cursor.getCount() > 0;
         cursor.close();
-
+        db.close();
         return usernameExists;
     }
 
-    public Boolean checkUsernamePassword(String username, String password) {
-        SQLiteDatabase myDatabase = this.getWritableDatabase();
-        Cursor cursor = myDatabase.rawQuery("SELECT * FROM users WHERE username = ? AND password = ?", new String[]{username, password});
+    public boolean checkUsernamePassword(User user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE username = ? AND password = ?", new String[]{user.username, user.password});
         boolean usernamePasswordMatch = cursor.getCount() > 0;
         cursor.close();
+        db.close();
         return usernamePasswordMatch;
     }
-
 }
+
